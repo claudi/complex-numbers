8c060fe2297c593fc57c54fd0fe07ab4fc58e7fd
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-12 21:10:54 +0100

    Move definitions to Lib.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,5 @@ module Main where
 
 import Lib
 
-type Re = Double
-type Im = Double
-
-type Norm = Double
-type Argument = Double
-
-data Complex = Cartesian Re Im
-             | Polar Norm Argument
-
-instance Show Complex where
-  show (Cartesian x y) = (show x) ++ " + " ++ (show y) ++ "i"
-  show (Polar     r t) = (show r) ++ "e^(" ++ (show t) ++ "i)"
-
 main :: IO ()
 main = someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,20 @@
 module Lib
     ( someFunc
+    , Complex (Cartesian, Polar)
     ) where
+
+type Re = Double
+type Im = Double
+
+type Norm = Double
+type Argument = Double
+
+data Complex = Cartesian Re Im
+             | Polar Norm Argument
+
+instance Show Complex where
+  show (Cartesian x y) = (show x) ++ " + " ++ (show y) ++ "i"
+  show (Polar     r t) = (show r) ++ "e^(" ++ (show t) ++ "i)"
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
