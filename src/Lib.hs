43d0fdc7de1aee2c5c64fd4fc4c190eaede513d0
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-12 21:14:43 +0100

    Add conjugate function

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,5 +16,9 @@ instance Show Complex where
   show (Cartesian x y) = (show x) ++ " + " ++ (show y) ++ "i"
   show (Polar     r t) = (show r) ++ "e^(" ++ (show t) ++ "i)"
 
+conjugate :: Complex -> Complex
+conjugate (Cartesian x y) = Cartesian x (-y)
+conjugate (Polar r t) = Polar r (-t)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
