7d35b328cda902a2cdd1f0c328fde4cec5a6cd74
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-19 16:11:04 +0100

    Apply hlint hints

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,14 +13,14 @@ data Complex = Cartesian Re Im
              | Polar Norm Argument
 
 instance Show Complex where
-  show (Cartesian x y) = (show x) ++ " + " ++ (show y) ++ "*i"
-  show (Polar     r t) = (show r) ++ "e^(" ++ (show t) ++ "*i)"
+  show (Cartesian x y) = show x ++ " + " ++ show y ++ "*i"
+  show (Polar     r t) = show r ++ "e^(" ++ show t ++ "*i)"
 
 mod2PI :: Argument -> Argument
 mod2PI t
-  | (t <= 2*pi) && (t >= 0) = t
-  | (t >= 2*pi)             = mod2PI (t - 2*pi)
-  | (t < 0)                 = mod2PI (t + 2*pi)
+  | t <= 2*pi && t >= 0 = t
+  | t >= 2*pi             = mod2PI (t - 2*pi)
+  | t < 0                 = mod2PI (t + 2*pi)
 
 simplifyNum :: Complex -> Complex
 simplifyNum (Cartesian x y) = Cartesian x y
@@ -39,11 +39,11 @@ arg (Polar _ t) = t
 
 toCartesian :: Complex -> Complex
 toCartesian (Cartesian x y) = Cartesian x y
-toCartesian (Polar r t) = Cartesian (r*cos(t)) (r*sin(t))
+toCartesian (Polar r t) = Cartesian (r * cos t) (r * sin t)
 
 toPolar :: Complex -> Complex
 toPolar z@(Cartesian _ _) = Polar (norm z) (arg z)
-toPolar   (Polar r t) = (Polar r t)
+toPolar   (Polar r t) = Polar r t
 
 conjugate :: Complex -> Complex
 conjugate (Cartesian x y) = Cartesian x (-y)
